1027b9bcd9af0bcb80c7bbbee07b247454fd01a1
Jyotsna-Singh <jyotsna_singh94@yahoo.co.in>
2017-08-21 10:09:50 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  items = ["Angular 4", "React", "Redux"];
+  newItem = "";
+  pushItem = function() {
+    if(this.newItem != ""){
+      this.items.push(this.newItem);
+      this.newItem = "";
+    }
+  }
+
+  removeItem = function(index) {
+    this.items.splice(index, 1);
+  }
 }
